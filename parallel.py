19c73724dc0807847d289413029948f40b77953e
Erik Fredericks <fredericks@oakland.edu>
2018-12-21 23:02:41 -0500

    Added seed/argparse to parallel script

--- parallel.py
+++ parallel.py
@@ -4,14 +4,22 @@ from subprocess import Popen
 
 from app import Boot
 import sys
+import argparse
+
+# Handle arguments
+parser = argparse.ArgumentParser(description='Executes CrowdNav application.')
+parser.add_argument('--seed', help='Initializes random seed. Random seed if left blank.', type=int)
+parser.add_argument('--processor_count', help='Number of processors to use.', type=int, default=4)
 
 # this script is used to run CrowdNav on multiple processes
-# call it with >parallel.py #threads
+# call it with >parallel.py --processor_count #threads
 if __name__ == "__main__":
-    try:
-        processorCount = int(sys.argv[1])
-    except:
-        processorCount = 4
-    for i in range(0, processorCount):
-        simulation = Popen(["python", "./run.py", str(i)])
-        print("Simulation " + str(i) + " started...")
+    args = parser.parse_args()
+
+    for i in range(0, args.processor_count):
+        if args.seed is not None:
+          simulation = Popen(["python", "./run.py", "--process_id", str(i), "--seed", str(args.seed)])
+          print("Simulation " + str(i) + " started - seed [%d]..." % args.seed)
+        else:
+          simulation = Popen(["python", "./run.py", "--process_id", str(i)])
+          print("Simulation " + str(i) + " started - random seed...")
