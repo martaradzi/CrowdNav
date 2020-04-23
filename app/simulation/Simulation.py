1df3bc2b464d80c8a73647d960b255e9e996248c
martaradzi <marta.radziszewska@gmail.com>
2020-04-23 15:10:57 +0200

    new car number

--- app/simulation/Simulation.py
+++ app/simulation/Simulation.py
@@ -141,32 +141,30 @@ class Simulation(object):
                     CarRegistry.totalTrips) + ")" + " # avgTripOverhead: " + str(
                     CarRegistry.totalTripOverheadAverage))
            
-            counter = 0
+            # counter = 0
             if (cls.tick % 5000) == 0 and Config.parallelMode is False:
-                if counter == 0 or counter == 9 or counter == 10 or counter == 19:
-                    CarRegistry.totalCarCounter = 200
-                elif counter == 1 or counter == 8 or counter == 11 or counter == 18:
-                    CarRegistry.totalCarCounter = 300
-                elif counter == 2 or counter == 7 or counter == 12 or counter == 17:
-                    CarRegistry.totalCarCounter = 400
-                elif counter == 3 or counter == 6 or counter == 13 or counter == 16:
-                    CarRegistry.totalCarCounter = 500
+                # if counter == 0 or counter == 9 or counter == 10 or counter == 19:
+                #     CarRegistry.totalCarCounter = 200
+                # elif counter == 1 or counter == 8 or counter == 11 or counter == 18:
+                #     CarRegistry.totalCarCounter = 300
+                # elif counter == 2 or counter == 7 or counter == 12 or counter == 17:
+                #     CarRegistry.totalCarCounter = 400
+                # elif counter == 3 or counter == 6 or counter == 13 or counter == 16:
+                #     CarRegistry.totalCarCounter = 500
+                # else:
+                #     CarRegistry.totalCarCounter = 600
+                # counter += 1
+                # CarRegistry.applyCarCounter()
+                carNumber = CarRegistry.totalCarCounter
+                if carNumber <= 200:
+                   CarRegistry.totalCarCounter += 100
+                elif carNumber >= 600:
+                   CarRegistry.totalCarCounter -= 100
                 else:
-                    CarRegistry.totalCarCounter = 600
-                counter += 1
-                CarRegistry.applyCarCounter()
-                #carNumber = CarRegistry.totalCarCounter
-                #if carNumber <= 200:
-                #    CarRegistry.totalCarCounter += 100
-                #elif carNumber >= 600:
-                #    CarRegistry.totalCarCounter -= 100
-                #else:
-                   # n = random.choice([(carNumber+100), (carNumber-100)])
-            #        # n = random.choice([(carNumber+random.randint(50,101)), (carNumber-random.randint(50,101))])
-            #        # CarRegistry.totalCarCounter = n
-                #CarRegistry.totalCarCounter =  random.choice([(carNumber+100), (carNumber-100)])
+                    # n = random.choice([(carNumber+random.randint(50,101)), (carNumber-random.randint(50,101))])
+                    CarRegistry.totalCarCounter =  random.choice([(carNumber+100), (carNumber-100)])
 
-#                CarRegistry.applyCarCounter()
+                CarRegistry.applyCarCounter()
 
             if (cls.tick % 10) == 0:
                 msg = dict()
